c6e98d350abc71959e8d6c39d64b9dd3e66994c7
itzsam2000 <itzsamhere20@gmail.com>
2023-01-11 23:07:45 +0500

    first commit

---
+++ src/components/hero/hero.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import "../hero/hero.css"
+export default function Hero() {
+    return (
+        <div>Hero</div>
+    )
+}
---
+++ src/components/videography/videography.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import "../videography/videography.css"
+export default function Videography() {
+    return (
+        <div>Videography</div>
+    )
+}
