5846b504b338ed5bfb699c3918400ffdf65d8205
itzsam2000 <itzsamhere20@gmail.com>
2023-01-13 04:37:54 +0500

    first commit

--- src/components/videography/videography.js
+++ src/components/videography/videography.js
@@ -1,7 +1,416 @@
 import React from 'react'
 import "../videography/videography.css"
+import Footer from '../footer/footer'
+import video1 from "../assets/videography/video1.mp4"
 export default function Videography() {
     return (
-        <div>Videography</div>
+
+        <>
+            {/*-------------------Main Container-----------------*/}
+
+            <div className='videography__container'>
+
+
+                {/*-------------------Video Heading -----------------*/}
+
+                <div className="video__heading">
+                    Selection of my recent work
+                </div>
+
+
+                {/*-------------------video Section-----------------*/}
+
+
+                <div className="video__section">
+
+                    {/*------------------- Video Data -----------------*/}
+                    <div className='video__data'>
+
+                        {/*------------------- 1st Video Side-----------------*/}
+                        <div className="video__side">
+
+                            <video
+                                className='video'
+                                loop
+                                muted
+                                autoPlay
+                                alt="All the devices"
+                                src={video1}
+                            />
+
+
+                        </div>
+
+                        {/*------------------- 1st Info side-----------------*/}
+
+                        <div className="video__info">
+
+                            <h3>
+                                The Man and his Vietnamese Brides
+                            </h3>
+                            <br />
+                            <br />
+                            <span>
+                                For Rice Media
+                                <br />
+                                PRODUCER, WRITER, DIRECTOR
+                            </span>
+                            <br />
+                            <br />
+                            <h4>
+                                Watch
+                                <a href='/' target='_blank'>Here</a>
+                            </h4>
+                            <br />
+                            <br />
+                            <p>
+                                What if love wasn't just about emotional connection, sexual attraction
+                                or sharing the same values... but an exchange of money?
+                                <br /><br />
+                                Some may call his matchmaking of male customers with Vietnamese women modern-day human trafficking.
+                                We take a deeper dive into Mark's unshakeable belief in his booming trade,
+                                who his customers are, and what they’re looking for in a life partner.
+                            </p>
+
+                        </div>
+
+                    </div>
+
+
+
+
+
+                    {/*-------------------------------------------   2nd Video ------------------------------------------*/}
+
+
+
+
+
+                    {/*------------------- 2nd Video Side-----------------*/}
+                    <div className='video__data'>
+                        <div className="video__side">
+
+                            <video
+                                className='video'
+                                loop
+                                muted
+                                autoPlay
+                                alt="All the devices"
+                                src={video1}
+                            />
+
+
+                        </div>
+
+                        {/*-------------------2nd Info side-----------------*/}
+
+                        <div className="video__info">
+
+                            <h3>
+                                The Man and his Vietnamese Brides
+                            </h3>
+                            <br />
+                            <br />
+                            <span>
+                                For Rice Media
+                                <br />
+                                PRODUCER, WRITER, DIRECTOR
+                            </span>
+                            <br />
+                            <br />
+                            <h4>
+                                Watch
+                                <a href='/' target='_blank'>Here</a>
+                            </h4>
+                            <br />
+                            <br />
+                            <p>
+                                What if love wasn't just about emotional connection, sexual attraction
+                                or sharing the same values... but an exchange of money?
+                                <br /><br />
+                                Some may call his matchmaking of male customers with Vietnamese women modern-day human trafficking.
+                                We take a deeper dive into Mark's unshakeable belief in his booming trade,
+                                who his customers are, and what they’re looking for in a life partner.
+                            </p>
+
+                        </div>
+
+                    </div>
+
+
+
+
+
+                    {/*-------------------------------------------   3rd Video ------------------------------------------*/}
+
+
+
+
+
+                    {/*-------------------  3rd Video Side-----------------*/}
+                    <div className='video__data'>
+                        <div className="video__side">
+
+                            <video
+                                className='video'
+                                loop
+                                muted
+                                autoPlay
+                                alt="All the devices"
+                                src={video1}
+                            />
+
+
+                        </div>
+
+                        {/*------------------- 3rd Info side-----------------*/}
+
+                        <div className="video__info">
+
+                            <h3>
+                                The Man and his Vietnamese Brides
+                            </h3>
+                            <br />
+                            <br />
+                            <span>
+                                For Rice Media
+                                <br />
+                                PRODUCER, WRITER, DIRECTOR
+                            </span>
+                            <br />
+                            <br />
+                            <h4>
+                                Watch
+                                <a href='/' target='_blank'>Here</a>
+                            </h4>
+                            <br />
+                            <br />
+                            <p>
+                                What if love wasn't just about emotional connection, sexual attraction
+                                or sharing the same values... but an exchange of money?
+                                <br /><br />
+                                Some may call his matchmaking of male customers with Vietnamese women modern-day human trafficking.
+                                We take a deeper dive into Mark's unshakeable belief in his booming trade,
+                                who his customers are, and what they’re looking for in a life partner.
+                            </p>
+
+                        </div>
+
+                    </div>
+
+
+
+
+
+
+
+
+
+                    {/*-------------------------------------------   4th Video ------------------------------------------*/}
+
+
+
+
+
+
+
+                    {/*-------------------  4th Video Side-----------------*/}
+                    <div className='video__data'>
+                        <div className="video__side">
+
+                            <video
+                                className='video'
+                                loop
+                                muted
+                                autoPlay
+                                alt="All the devices"
+                                src={video1}
+                            />
+
+
+                        </div>
+
+                        {/*------------------- 4th Info side-----------------*/}
+
+                        <div className="video__info">
+
+                            <h3>
+                                The Man and his Vietnamese Brides
+                            </h3>
+                            <br />
+                            <br />
+                            <span>
+                                For Rice Media
+                                <br />
+                                PRODUCER, WRITER, DIRECTOR
+                            </span>
+                            <br />
+                            <br />
+                            <h4>
+                                Watch
+                                <a href='/' target='_blank'>Here</a>
+                            </h4>
+                            <br />
+                            <br />
+                            <p>
+                                What if love wasn't just about emotional connection, sexual attraction
+                                or sharing the same values... but an exchange of money?
+                                <br /><br />
+                                Some may call his matchmaking of male customers with Vietnamese women modern-day human trafficking.
+                                We take a deeper dive into Mark's unshakeable belief in his booming trade,
+                                who his customers are, and what they’re looking for in a life partner.
+                            </p>
+
+                        </div>
+
+                    </div>
+
+
+
+
+
+
+
+                    {/*-------------------------------------------   5th Video ------------------------------------------*/}
+
+
+
+
+
+
+
+                    {/*------------------- 5th  Video Side-----------------*/}
+                    <div className='video__data'>
+                        <div className="video__side">
+
+                            <video
+                                className='video'
+                                loop
+                                muted
+                                autoPlay
+                                alt="All the devices"
+                                src={video1}
+                            />
+
+
+                        </div>
+
+                        {/*------------------- 5th Info side-----------------*/}
+
+                        <div className="video__info">
+
+                            <h3>
+                                The Man and his Vietnamese Brides
+                            </h3>
+                            <br />
+                            <br />
+                            <span>
+                                For Rice Media
+                                <br />
+                                PRODUCER, WRITER, DIRECTOR
+                            </span>
+                            <br />
+                            <br />
+                            <h4>
+                                Watch
+                                <a href='/' target='_blank'>Here</a>
+                            </h4>
+                            <br />
+                            <br />
+                            <p>
+                                What if love wasn't just about emotional connection, sexual attraction
+                                or sharing the same values... but an exchange of money?
+                                <br /><br />
+                                Some may call his matchmaking of male customers with Vietnamese women modern-day human trafficking.
+                                We take a deeper dive into Mark's unshakeable belief in his booming trade,
+                                who his customers are, and what they’re looking for in a life partner.
+                            </p>
+
+                        </div>
+
+                    </div>
+
+
+
+
+
+
+
+                    {/*-------------------------------------------   6th Video ------------------------------------------*/}
+
+
+
+
+
+
+
+                    {/*-------------------  6th Video Side-----------------*/}
+                    <div className='video__data'>
+                        <div className="video__side">
+
+                            <video
+                                className='video'
+                                loop
+                                muted
+                                autoPlay
+                                alt="All the devices"
+                                src={video1}
+                            />
+
+
+                        </div>
+
+                        {/*------------------- 6th Info side-----------------*/}
+
+                        <div className="video__info">
+
+                            <h3>
+                                The Man and his Vietnamese Brides
+                            </h3>
+                            <br />
+                            <br />
+                            <span>
+                                For Rice Media
+                                <br />
+                                PRODUCER, WRITER, DIRECTOR
+                            </span>
+                            <br />
+                            <br />
+                            <h4>
+                                Watch
+                                <a href='/' target='_blank'>here</a>
+                            </h4>
+                            <br />
+                            <br />
+                            <p>
+                                What if love wasn't just about emotional connection, sexual attraction
+                                or sharing the same values... but an exchange of money?
+                                <br /><br />
+                                Some may call his matchmaking of male customers with Vietnamese women modern-day human trafficking.
+                                We take a deeper dive into Mark's unshakeable belief in his booming trade,
+                                who his customers are, and what they’re looking for in a life partner.
+                            </p>
+
+                        </div>
+
+
+
+                    </div>
+
+
+
+                    {/*------------------- End of Video section -----------------*/}
+
+                </div>
+
+                {/*-------------------End of Main Container-----------------*/}
+
+            </div>
+
+            {/*-------------------Footer-----------------*/}
+
+            <Footer />
+        </>
+
     )
 }
